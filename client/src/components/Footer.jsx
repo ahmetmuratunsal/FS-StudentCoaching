e6d32a5a281f305bd3fb4f80d4f0106bab6b6b7d
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-06-15 12:33:01 +0300

    basic css updated

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="w-full max-w-[1440px] mx-auto p-5 border-t text-center">
+    <div className="w-full max-w-[1440px] mx-auto p-5 border-t text-center mt-20">
       Tüm Hakları Saklıdır. &copy;ByAhmet
     </div>
   );
