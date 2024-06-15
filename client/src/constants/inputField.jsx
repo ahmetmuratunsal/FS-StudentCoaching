0203f5004066c20fe2a41922598d3172d31ddd15
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-06-15 12:33:45 +0300

    profilePhoto field added.

--- client/src/constants/inputField.jsx
+++ client/src/constants/inputField.jsx
@@ -23,7 +23,7 @@ export const inputField = [
   },
   {
     label: "İsminiz",
-    name: "firstname",
+    name: "firstName",
     placeholder: "İsminizi giriniz",
     type: "text",
     required: true,
@@ -45,7 +45,7 @@ export const inputField = [
   },
   {
     label: "Soyisminiz",
-    name: "surname",
+    name: "lastName",
     placeholder: "Soyisminizi giriniz",
     type: "text",
     required: true,
@@ -119,6 +119,40 @@ export const inputField = [
           d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
           data-original="#000000"
         ></path>
+      </svg>
+    ),
+  },
+
+  {
+    label: "Profil Fotoğrafı",
+    name: "profilePhoto",
+    placeholder: "Fotoğraf",
+    type: "file",
+    required: false,
+    svg: (
+      <svg
+        viewBox="0 0 24 24"
+        className="w-[18px] h-[18px] absolute right-2 cursor-pointer"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        stroke="#c4c4c4"
+      >
+        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+        <g
+          id="SVGRepo_tracerCarrier"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></g>
+        <g id="SVGRepo_iconCarrier">
+          {" "}
+          <path
+            d="M3 10H4C4.93188 10 5.39782 10 5.76537 10.1522C6.25542 10.3552 6.64477 10.7446 6.84776 11.2346C7 11.6022 7 12.0681 7 13C7 13.9319 7 14.3978 6.84776 14.7654C6.64477 15.2554 6.25542 15.6448 5.76537 15.8478C5.39782 16 4.93188 16 4 16H3M19 6V5C19 3.89543 18.1046 3 17 3H15C13.8954 3 13 3.89543 13 5V6M6.2 20H17.8C18.9201 20 19.4802 20 19.908 19.782C20.2843 19.5903 20.5903 19.2843 20.782 18.908C21 18.4802 21 17.9201 21 16.8V9.2C21 8.07989 21 7.51984 20.782 7.09202C20.5903 6.71569 20.2843 6.40973 19.908 6.21799C19.4802 6 18.9201 6 17.8 6H6.2C5.0799 6 4.51984 6 4.09202 6.21799C3.71569 6.40973 3.40973 6.71569 3.21799 7.09202C3 7.51984 3 8.07989 3 9.2V16.8C3 17.9201 3 18.4802 3.21799 18.908C3.40973 19.2843 3.71569 19.5903 4.09202 19.782C4.51984 20 5.07989 20 6.2 20ZM17 13C17 14.6569 15.6569 16 14 16C12.3431 16 11 14.6569 11 13C11 11.3431 12.3431 10 14 10C15.6569 10 17 11.3431 17 13Z"
+            stroke="#c4c4c4"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>{" "}
+        </g>
       </svg>
     ),
   },
