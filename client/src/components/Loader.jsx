c18b9ea68e52a79afb40598c059aa7dca87ae48b
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-06-18 10:21:22 +0300

    loader component added

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="flex flex-row gap-2 justify-center my-32">
+      <div className="loaderCSS w-14 h-14 rounded-full bg-radial bg-gradient-to-tr animate-spin [animation-delay:.7s]"></div>
+    </div>
+  );
+};
+
+export default Loader;
