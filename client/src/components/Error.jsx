a394c24d0af73c4a235370dc97a5601967dd3945
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-06-18 12:15:53 +0300

    error page created.

---
+++ client/src/components/Error.jsx
@@ -0,0 +1,11 @@
+const Error = ({ isError }) => {
+  return (
+    <div className="flex flex-col items-center gap-2 my-10">
+      <h1 className=" font-bold text-3xl text-red-500">{isError.statusCode}</h1>
+      <h2 className=" text-xl">Üzgünüz bir hata oluştu :(</h2>
+      <p className=" text-xl">{isError.message}</p>
+    </div>
+  );
+};
+
+export default Error;
