a883a106850b8c98635c426f3fa7822ff6937895
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-06-18 10:21:13 +0300

    search input added.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import api from "../utils/api";
+import { CiSearch } from "react-icons/ci";
 
 const Header = () => {
   const user = JSON.parse(localStorage.getItem("user"));
@@ -17,12 +18,35 @@ const Header = () => {
         toast.error(`Çıkış yapılırken bir sorun oluştu ${err.message}`)
       );
   };
+  const handleSearch = (e) => {
+    //* sayfa yenileme engelle
+    e.preventDefault();
+    //* aratılan kelimeyi al
+    const text = e.target[0].value;
+    //* kullanıcıyı özel ders sayfasına yönlendir.
+    navigate(`/search/privatelesson?query=${text}`);
+    e.target[0].value = "";
+  };
   return (
     <header className="p-5 shadow">
-      <div className="max-w-[1440px] mx-auto flex justify-between">
+      <div className="max-w-[1440px] mx-auto flex justify-between items-center gap-4 md:gap-8">
         <Link to={"/"}>
-          <img className="w-[100px]" src="logo.png" alt="logo" />
+          <img className="w-[100px]" src="/logo.png" alt="logo" />
         </Link>
+
+        <form
+          onSubmit={handleSearch}
+          className="rounded-md flex  flex-1 items-center border max-w-[600px]"
+        >
+          <input
+            className="w-full h-full px-3 outline-none "
+            placeholder="Ara"
+            type="search"
+          />
+          <button className="bg-black  p-2 rounded-md text-white text-xl max-md:hidden">
+            <CiSearch />
+          </button>
+        </form>
 
         <div className="flex items-center gap-2 group relative">
           {user ? (
