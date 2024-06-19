47cdcbaa3fcb199f31e14431d45238e8e1990c2e
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-06-19 11:08:47 +0300

    PrivateLessonDetail page added.

---
+++ client/src/pages/PrivateLessonDetail/index.jsx
@@ -0,0 +1,50 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getPrivateLesson } from "../../redux/privateLessonSlice/privateLessonActions";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../../components/Loader";
+import Error from "../../components/Error";
+import Info from "./Info";
+import OrderBox from "./OrderBox";
+import TeacherInfo from "./TeacherInfo";
+import Reviews from "./Reviews";
+
+const PrivateLessonDetail = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const { isLoading, isError, onePrivateLesson } = useSelector(
+    (store) => store.privateLesson
+  );
+
+  const lesson = onePrivateLesson.privateLesson;
+
+  console.log(lesson);
+  useEffect(() => {
+    dispatch(getPrivateLesson(id));
+  }, [id]);
+
+  return (
+    <div>
+      {isLoading ? (
+        <Loader />
+      ) : isError ? (
+        <Error isError={isError} />
+      ) : (
+        <div className="flex flex-col lg:flex-row gap-10">
+          <div>
+            {/* Info detaylar kısmı */}
+            <Info lesson={lesson} />
+            {/* Teacher detaylar kısmı */}
+            <TeacherInfo teacher={lesson?.teacher} />
+            {/* Yorumlar kısmı */}
+            <Reviews />
+          </div>
+          {/* Satın alma kısmı */}
+          <OrderBox lesson={lesson} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PrivateLessonDetail;
