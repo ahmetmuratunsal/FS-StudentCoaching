859d64842173cc547722af5e4e6ebeae10f7a26c
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-06-19 11:10:01 +0300

    PrivateLessonPage teacherInfo section added

---
+++ client/src/pages/PrivateLessonDetail/TeacherInfo.jsx
@@ -0,0 +1,64 @@
+import { FaStar } from "react-icons/fa";
+
+const TeacherInfo = ({ teacher }) => {
+  return (
+    <>
+      <div>
+        <h1 className="text-lg font-bold mt-5">Öğretmen Hakkında</h1>
+
+        <div className="flex items-center gap-2 mt-4">
+          <img
+            className="w-[70px] h-[70px] rounded-full object-cover"
+            src={teacher?.profilePhoto}
+            alt={teacher?.username}
+          />
+          <div className="flex flex-col gap-1">
+            <h4 className="capitalize text-lg font-bold">
+              {teacher?.username}
+            </h4>
+            <div className="flex gap-1 text-yellow-400">
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaStar />
+            </div>
+            <button className="text-sm rounded-md py-1 px-2 border">
+              İletişime Geç
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="border mt-5 p-5">
+        <p className="flex flex-col gap-2">
+          <span>Otobiografi</span>
+          <span>{teacher?.bio}</span>
+        </p>
+
+        <div className="grid md:grid-cols-2 gap-5 mt-4">
+          <p className="flex flex-col gap-1 ">
+            <span>İsim Soyisim</span>
+            <span className="font-bold capitalize">
+              {teacher?.firstName} {teacher?.lastName}
+            </span>
+          </p>
+          <p className="flex flex-col gap-1">
+            <span>Hangi Şehirdeyim?</span>
+            <span className="font-bold capitalize">{teacher?.city}</span>
+          </p>
+
+          <p className="flex flex-col gap-1">
+            <span>Ders yeri tercihim?</span>
+            <span className="font-bold capitalize">{teacher?.lessonType}</span>
+          </p>
+
+          <p className="flex flex-col gap-1">
+            <span>Üyelik Tarihi</span>
+            <span className="font-bold capitalize">{teacher?.createdAt}</span>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TeacherInfo;
