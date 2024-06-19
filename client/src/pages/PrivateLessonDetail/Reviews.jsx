e3d126e4d7e98a73a6790567de60d7ce80b3b0ca
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-06-19 11:09:29 +0300

    PrivateLessonPage reviews section added

---
+++ client/src/pages/PrivateLessonDetail/Reviews.jsx
@@ -0,0 +1,15 @@
+import Review from "./Review";
+
+const Reviews = () => {
+  return (
+    <div className="my-10">
+      <h1 className="text-xl font-bold">Yorumlar</h1>
+      <Review />
+      <Review />
+      <Review />
+      <Review />
+    </div>
+  );
+};
+
+export default Reviews;
