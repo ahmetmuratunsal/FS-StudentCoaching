6b4038698ab169b57bd9f74109f0af52b4b3453d
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-06-22 09:10:48 +0300

    reviews reducer added store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import privateLessonReducer from "./privateLessonSlice/privateLessonSlice";
+import reviewReducer from "./reviewSlice/reviewSlice";
 
 export const store = configureStore({
   reducer: {
     privateLesson: privateLessonReducer,
+    reviews: reviewReducer,
   },
 });
