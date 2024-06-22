cd71249c7d1c05d2153b84b63b05e8f34d7e607b
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-06-22 09:11:49 +0300

    photo updated func added.

---
+++ client/src/utils/upload.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+export const upload = async (file, collection) => {
+  // resim olduğunu kontrol et resim değilse hata ver
+  if (!file.type.startsWith("image")) return null;
+
+  // resmi bir form daha içerisine ekle
+
+  const data = new FormData();
+
+  data.append("file", file);
+  // yüklenme ayarlarını belirle
+
+  data.append("upload_preset", collection);
+
+  try {
+    // api isteği atıp resmi buluta yükle
+    const res = await axios.post(
+      "https://api.cloudinary.com/v1_1/dlcwbxs5c/image/upload",
+      data
+    );
+
+    // resmin urlini fonksiyonun çağrıldığı yere döndür
+
+    return res.data.url;
+  } catch (error) {
+    toast.error(`Fotoğraf yüklenirken bir hata oluştu. ${error.message}`);
+  }
+};
