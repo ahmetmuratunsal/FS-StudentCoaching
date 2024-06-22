c6c2d25793c8e78d1deb58f4b2d6f56b0883c811
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-06-22 09:07:13 +0300

    özel ders ekleme sayfasındaki selectlerin optionsları eklendi.

---
+++ client/src/constants/addPrivateLessonSelect.js
@@ -0,0 +1,24 @@
+export const featureOptions = [
+  { value: "feature1", label: "Döküman Desteği" },
+  { value: "feature2", label: "Sürekli Soru İmkanı" },
+  { value: "feature3", label: "Koçluk Hizmeti" },
+  { value: "feature4", label: "Deneme Takibi" },
+  // Diğer özellikler burada eklenebilir
+];
+
+export const categoryOptions = [
+  { value: "studentCoaching", label: "Öğrenci Koçluğu" },
+  { value: "math", label: "Matematik" },
+  { value: "fen", label: "Fen Bilimleri" },
+  { value: "turkish", label: "Türkçe" },
+  { value: "fast", label: "Hızlı Okuma" },
+  // Diğer özellikler burada eklenebilir
+];
+
+export const lessonTypeOptions = [
+  { value: "remote", label: "Uzaktan" },
+  { value: "teachersHome", label: "Öğretmen Evi" },
+  { value: "studentsHome", label: "Öğrenci Evi" },
+  { value: "library", label: "Kütüphane" },
+  // Diğer özellikler burada eklenebilir
+];
