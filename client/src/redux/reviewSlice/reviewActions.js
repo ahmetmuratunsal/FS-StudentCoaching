76d635488fd44db45c107bc03357dca306024bcf
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-06-23 15:49:19 +0300

    createReview actions added.

--- client/src/redux/reviewSlice/reviewActions.js
+++ client/src/redux/reviewSlice/reviewActions.js
@@ -15,3 +15,18 @@ export const getAllReviews = createAsyncThunk(
     }
   }
 );
+
+//* async şekilde yeni özel ders ekleme
+export const createReview = createAsyncThunk(
+  "reviews/createReview",
+  async (newReview, { rejectWithValue }) => {
+    try {
+      // apiden cevap geldiyse return ile slicea gönder
+      const res = await api.post(`/review`, newReview);
+      return res.data.review;
+    } catch (err) {
+      // API'den gelen hatayı rejectWithValue ile slice'a gönder
+      return rejectWithValue(err.response.statusText);
+    }
+  }
+);
