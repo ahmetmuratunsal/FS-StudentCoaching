3d2cb695fbd557a10ac6bef03a9b4605e3a1ab4f
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-06-23 15:50:24 +0300

    Security Middleware added

--- api/server.js
+++ api/server.js
@@ -7,6 +7,11 @@ import reviewRouter from "./routes/review.route.js";
 import cors from "cors";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import AppError from "./utils/appError.js";
+import mongoSanitize from "express-mongo-sanitize";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+
 //* env dosyasındaki verilere erişmek için kurulum
 dotenv.config();
 
@@ -23,9 +28,8 @@ const app = express();
 
 //! middlewares
 
-//*a) bodydeki json içeriğinin okunmasını sağlar
-
-app.use(express.json());
+//*a) body headers vs. gelen json verisini js'de kullanbilir formata getirir
+app.use(express.json({ limit: "10kb" }));
 
 //*b) çerezleri işler ulaşmamızı sağlar
 
@@ -39,21 +43,58 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(morgan("dev"));
 
+//! güvenlik ile alakalı middlewareler
+
+//* güvenlik için headerlar ekler
+app.use(helmet());
+
+//* bir ip adresinden belirli süre içerinde gelicek olan istekleri sınırla
+const limiter = rateLimit({
+  max: 100, //* aynı ip adresinden gelicek maks istek sınırı
+  windowMs: 60 * 60 * 1000, //* ms cinsinden 1 saat
+  message:
+    "1 saat içerisndeki istek hakkınızı doldurdunuz.Daha sonra tekrar deneyiniz",
+});
+
+// middleware'i api route'ları için tanıtma
+//TODO app.use("/api", limiter); api limitlerini aktif edeceğiz
+
+//* Data sanitization - Verileri Sterelize Etme - Query Injection
+//* isteğin body / params / header kısmına eklenen her türlü opeatörü  kaldır
+app.use(mongoSanitize());
+
 //? route tanımlama
 
 app.use("/api/auth", authRouter);
 app.use("/api/privatelesson", privateLessonRouter);
 app.use("/api/review", reviewRouter);
 
+// tanımlanmayan bir route istek atıldığında hata ver
+app.all("*", (req, res, next) => {
+  // hata detaylarını belirle
+  const error = new AppError("Tanımlanmyan bir route'a istek attınız", 404);
+
+  // hata middlwareine yönlendir ve hata bilgilerini gönder
+  next(error);
+});
+
 //? hata yönetimi
 //* controllerda yapılacak tüm yönlendirmeler bu middlewarei tetikler.
+// hata olduğunda devreye giren bir middlware
+// hata bilgilerini alır ve cevap olarak gönderir
 app.use((err, req, res, next) => {
-  const errStatus = err.status || 500;
-  const errMessage = err.message || "Üzgünüz bir şeyler ters gitti";
+  // hata detaylarını konsola yaz
+  console.log(err.stack);
 
-  return res.status(errStatus).json({
-    statusCode: errStatus,
-    message: errMessage,
+  // durum kodu veya durum değerleri gönderilmediğinde varsayılan değerler devreye girsin
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  err.message = err.message || "Üzgünüz bir hata oluştu";
+
+  // cevap gönder
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
