6d23c88e351386d8d5a77a232942a51e5b08121e
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-06-25 09:09:15 +0300

    layout added

---
+++ client/src/Layout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const Layout = () => (
+  <div className="flex flex-col min-h-screen">
+    <Header />
+    <div className="flex-1 max-w-[1440px] mx-auto p-5 w-full">
+      <Outlet />
+    </div>
+    <Footer />
+  </div>
+);
+
+export default Layout;
