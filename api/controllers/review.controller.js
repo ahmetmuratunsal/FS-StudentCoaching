8abb530d6e1850ab5159b866519777e68df69fb7
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-06-25 09:08:01 +0300

    app Error setup

--- api/controllers/review.controller.js
+++ api/controllers/review.controller.js
@@ -1,11 +1,14 @@
-import error from "../utils/error.js";
 import Review from "../models/review.model.js";
 import privateLessonModel from "../models/privateLesson.model.js";
+import reviewModel from "../models/review.model.js";
+import AppError from "../utils/appError.js";
+import { factoryGetAll } from "./handlerFactory.js";
 
-//* yorum ekleme
+//! yorum ekleme
 export const createReview = async (req, res, next) => {
-  //* 1) kullanıcı satıcı ise işlemi iptal et (öğretmen yorum yapamaz)
-  if (!req.isStudent) return next(error(403, "Öğretmenler yorum atamaz."));
+  //* 1) kullanıcı öğretmen ise işlemi iptal et (öğretmen yorum yapamaz)
+  if (!req.isStudent)
+    return next(new AppError("Öğretmenler yorum atamaz.", 403));
 
   //* 2) yorum belgesi oluştur.
   const newReview = new Review({
@@ -24,7 +27,9 @@ export const createReview = async (req, res, next) => {
 
     //* 4) eğerki eski bir yorum varsa işlemi iptal et (her öğrenci 1 yorum atabilir)
     if (oldReview)
-      return next(error(403, "Daha önce bu özel derse bir yorum atmıştınız."));
+      return next(
+        new AppError("Daha önce bu özel derse bir yorum atmıştınız.", 403)
+      );
 
     //* 5) eski yorum yoksa yorumu kaydet
     await newReview.save();
@@ -42,11 +47,15 @@ export const createReview = async (req, res, next) => {
     });
   } catch (err) {
     console.log(err);
-    next(error(500, "Yorum gönderilirken bir sorun oluştu."));
+    next(new AppError("Yorum gönderilirken bir sorun oluştu.", 500));
   }
 };
 
-//* bir özel derse ait yorumları al
+//! Bütün yorumları al
+
+export const getAllReviews = factoryGetAll(reviewModel);
+
+//! bir özel derse ait yorumları al
 export const getReviews = async (req, res, next) => {
   try {
     const reviews = await Review.find({
@@ -54,7 +63,35 @@ export const getReviews = async (req, res, next) => {
     }).populate("student");
     res.status(200).json({ reviews });
   } catch (err) {
-    next(error(500, "Yorumlar alınırken bir sorun oluştu."));
+    next(new AppError("Yorumlar alınırken bir sorun oluştu.", 500));
   }
 };
-export const deleteReview = (req, res, next) => {};
+
+//! Yorumu silme
+
+export const deleteReview = async (req, res, next) => {
+  try {
+    //* 1) parametre olarak gönderilen idye eriş (req.params.id)
+    //* yorumun detaylarını al
+    const review = await reviewModel.findById(req.params.id);
+
+    //* 2) yorumu oluşturan ve silmek isteyen kullanıcı aynı mı kontrol et
+
+    if (review.teacher != req.userId)
+      return next(
+        new AppError("Sadece kendi oluşturduğunuz yorumu silebilirsiniz.", 403)
+      );
+
+    //* 3) özel dersi sil
+
+    await reviewModel.findByIdAndDelete(req.params.id);
+
+    //* 4) client'e cevap gönder
+    res.status(200).json({
+      message: "Yorum başarıyla kaldırıldı.",
+    });
+  } catch (err) {
+    console.log(err);
+    return next(new AppError("Yorum silinirken bir sorun oluştu.", 500));
+  }
+};
