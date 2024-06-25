f18cac98bd9f7d5302727e2756d86a55548218e6
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-06-25 09:08:39 +0300

    getAllReviews route added

--- api/routes/review.route.js
+++ api/routes/review.route.js
@@ -3,12 +3,14 @@ import {
   createReview,
   getReviews,
   deleteReview,
+  getAllReviews,
 } from "../controllers/review.controller.js";
 import protect from "../middlewares/protect.js";
 
 const router = express.Router();
 
 router.post("/", protect, createReview);
+router.get("/", getAllReviews);
 router.get("/:privateLessonId", getReviews);
 router.delete("/:privateLessonId", protect, deleteReview);
 
