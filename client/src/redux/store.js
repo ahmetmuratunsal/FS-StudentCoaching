becb65b66fceeb2b5fe2b01112cf2272612e262f
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-06-26 16:55:00 +0300

    add question slice

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import privateLessonReducer from "./privateLessonSlice/privateLessonSlice";
 import reviewReducer from "./reviewSlice/reviewSlice";
+import questionReducer from "./questionSlice/questionSlice";
 
 export const store = configureStore({
   reducer: {
     privateLesson: privateLessonReducer,
     reviews: reviewReducer,
+    question: questionReducer,
   },
 });
