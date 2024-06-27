2e8f6fa6d36830a69fbf4c7aedd810b5f3b9f288
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-06-27 12:46:20 +0300

    getOneQuestion added

--- client/src/redux/questionSlice/questionActions.js
+++ client/src/redux/questionSlice/questionActions.js
@@ -1,13 +1,28 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import api from "../../utils/api";
 
-//* async şekilde bütün özel dersleri alma
+//* async şekilde bütün soruları alma
 export const getAllQuestion = createAsyncThunk(
   "question/getAllQuestion",
   async (parametre, { rejectWithValue }) => {
     try {
       // apiden cevap geldiyse return ile slicea gönder
       const res = await api.get(`/question`, { params: parametre });
+      return res.data;
+    } catch (err) {
+      // API'den gelen hatayı rejectWithValue ile slice'a gönder
+      return rejectWithValue(err.response.statusText);
+    }
+  }
+);
+
+//* async şekilde tek bir soruyu alma
+export const getOneQuestion = createAsyncThunk(
+  "question/getOneQuestion",
+  async (id, { rejectWithValue }) => {
+    try {
+      // apiden cevap geldiyse return ile slicea gönder
+      const res = await api.get(`/question/${id}`);
       return res.data;
     } catch (err) {
       // API'den gelen hatayı rejectWithValue ile slice'a gönder
