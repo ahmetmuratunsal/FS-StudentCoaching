16297ca4bf4c114fd79d51dcdc3c8209e248a03b
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-06-28 14:56:20 +0300

    error status updated

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -1,9 +1,9 @@
 const Error = ({ isError }) => {
   return (
     <div className="flex flex-col items-center gap-2 my-10">
-      <h1 className=" font-bold text-3xl text-red-500">{isError.statusCode}</h1>
+      <h1 className=" font-bold text-3xl text-red-500">{isError?.status}</h1>
       <h2 className=" text-xl">Üzgünüz bir hata oluştu :(</h2>
-      <p className=" text-xl">{isError.message}</p>
+      <p className=" text-xl">{isError?.message}</p>
     </div>
   );
 };
