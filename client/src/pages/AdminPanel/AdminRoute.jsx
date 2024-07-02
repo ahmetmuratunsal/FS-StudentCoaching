1bbe8b1718ea86ccca6a2fefb0a4016517a1da4b
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-07-02 11:43:18 +0300

    adminRoute created and protected

---
+++ client/src/pages/AdminPanel/AdminRoute.jsx
@@ -0,0 +1,29 @@
+import { Navigate, Outlet } from "react-router-dom";
+import Footer from "../../components/Footer";
+import AdminSidebar from "./AdminSidebar";
+
+//! bu korumalı route sayesinde öğretmenler öğrencilerin özel sayfalarına giremeyecekler
+const AdminRoute = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  if (!user) return <Navigate to={"/"} replace />;
+
+  if (!user.isAdmin) {
+    return <Navigate to={"/"} replace />;
+  } else {
+    return (
+      <div className="flex flex-1 ">
+        <AdminSidebar />
+        <div className="flex flex-col gap-5 w-full h-min-screen">
+          <div className="flex-1 p-14">
+            <Outlet />
+          </div>
+          <div>
+            <Footer />
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default AdminRoute;
