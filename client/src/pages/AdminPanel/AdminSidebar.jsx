96a1223e2ad3298355e264966ac79f908c920a76
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-07-04 01:06:18 +0300

    admin questions link added

--- client/src/pages/AdminPanel/AdminSidebar.jsx
+++ client/src/pages/AdminPanel/AdminSidebar.jsx
@@ -166,7 +166,7 @@ const AdminSidebar = () => {
           </button>
           <div className=" flex justify-start  flex-col w-full text-sm lg:text-base md:w-auto items-start pb-1">
             <Link
-              to={"/studentquestions"}
+              to={"/adminquestions"}
               className="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2  w-full md:w-52"
             >
               <VscQuestion className=" lg:text-2xl" />
