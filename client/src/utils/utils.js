6470dbf77f3977318d258fbe588347c35c726bf5
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-07-03 12:04:25 +0300

    changeCityName added.

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,7 +1,19 @@
-import { lessonOptions } from "./../constants/selectInput";
+import { cityOptions, lessonOptions } from "./../constants/selectInput";
 
 /* apiden gelen ders verisini türkçe karekterleri haline çeviriyoruz */
 export const changeCategoryName = (category) => {
   const foundLesson = lessonOptions.find((i) => i.value === category);
+  if (!foundLesson) {
+    return category;
+  }
   return foundLesson.label;
+};
+
+/* apiden gelen city verisini türkçe karekterleri haline çeviriyoruz */
+export const changeCityName = (city) => {
+  const foundCity = cityOptions.find((i) => i.value === city);
+  if (!foundCity) {
+    return city;
+  }
+  return foundCity.label;
 };
