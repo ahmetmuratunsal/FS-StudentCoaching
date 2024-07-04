5b98e48deeae2f4b42b2ed11d7f8c48c07b9f161
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-07-04 11:16:32 +0300

    teachermeetings link added

--- client/src/pages/TeacherPanel/TeacherSidebar.jsx
+++ client/src/pages/TeacherPanel/TeacherSidebar.jsx
@@ -177,7 +177,10 @@ const TeacherSidebar = () => {
 
               <p className="text-base leading-4  ">Sorularım</p>
             </Link>
-            <Link className="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2  w-full md:w-52">
+            <Link
+              to={"/teachermeetings"}
+              className="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2  w-full md:w-52"
+            >
               <SiGooglemeet className="text-base lg:text-2xl" />
 
               <p className="text-base leading-4  ">Randevularım</p>
