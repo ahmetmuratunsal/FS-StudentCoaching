1ea10826f3e7ce3d02fb6095bc1dffff6c4fa0d5
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-07-04 23:18:38 +0300

    adminmeeting route link added

--- client/src/pages/AdminPanel/AdminSidebar.jsx
+++ client/src/pages/AdminPanel/AdminSidebar.jsx
@@ -171,8 +171,18 @@ const AdminSidebar = () => {
             >
               <VscQuestion className=" lg:text-2xl" />
 
-              <p className=" leading-4  ">Sorular</p>
+              <p className=" leading-4  ">Bütün Sorular</p>
             </Link>
+
+            <Link
+              to={"/adminmeetings"}
+              className="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2  w-full md:w-52"
+            >
+              <SiGooglemeet className=" lg:text-2xl" />
+
+              <p className=" leading-4 whitespace-nowrap ">Bütün Randevular</p>
+            </Link>
+
             <Link
               to={"/users"}
               className="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2  w-full md:w-52"
