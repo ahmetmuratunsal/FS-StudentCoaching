b1b637ede471361a7f905d278df7b957a8454eeb
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-07-31 08:47:38 +0300

    register password open close

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+import { MdVisibility, MdVisibilityOff } from "react-icons/md";
 const Input = ({ name, type = "text", placeholder, svg = "", label }) => {
-  return (
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+  return name === "password" ? (
+    <div className="relative mt-5">
+      <label htmlFor="password" className="text-sm font-bold block mb-2">
+        Şifreniz
+      </label>
+      <input
+        id="password"
+        name="password"
+        type={showPassword ? "text" : "password"}
+        placeholder="Şifrenizi giriniz"
+        className="w-full bg-transparent  text-sm border-b border-gray-300 focus:border-yellow-400 px-2 py-3 outline-none"
+      />
+      <div
+        className="absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
+        onClick={handleTogglePassword}
+      >
+        {showPassword ? (
+          <MdVisibilityOff size={20} color="#bbb" />
+        ) : (
+          <MdVisibility size={20} color="#bbb" />
+        )}
+      </div>
+    </div>
+  ) : (
     <div className="mt-5">
       <label className="text-sm font-bold block mb-2">{label}</label>
       <div className="relative flex items-center">
